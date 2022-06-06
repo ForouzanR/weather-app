e2a7b3bce2fd5a6efbefaec2d2e3eb132c717b61
ForouzanR <forouzan.rl@gmail.com>
2022-06-06 19:05:28 +0430

    java script part one
    adding the temperature ,etc.

---
+++ src/app.js
@@ -0,0 +1,18 @@
+function displayTemprature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement=document.querySelector("#humidity");
+  let windElement=document.querySelector("#wind"); 
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML=response.data.main.humidity;
+  windElement.innerHTML=Math.round(response.data.wind.speed);
+
+
+}
+let apiKey = "4daf684d6c6e93e1c6db6b899c513003";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemprature);
