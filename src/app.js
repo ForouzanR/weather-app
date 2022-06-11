3119a441cfa88881e1108e3f555337048b4fe356
ForouzanR <forouzan.rl@gmail.com>
2022-06-11 13:28:54 +0430

    editing

--- src/app.js
+++ src/app.js
@@ -20,10 +20,10 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day}, ${hours} : ${minutes}`;
 }
-function formatDay(timestamp){
-  let date=new Date(timestamp * 1000);
-  let day=date.getDay();
-  let days=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
   return days[day];
 }
 function displayForecast(response) {
@@ -32,24 +32,31 @@ function displayForecast(response) {
 
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index<6){
-    forecastHTML =
-      forecastHTML +
-      `
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
       <div class="col-2">
         <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         <img
-          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
           alt=""
           width="42"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}° /</span>
-          <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}° </span>
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° /</span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
         </div>
       </div>
   `;
-  }});
+    }
+  });
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
